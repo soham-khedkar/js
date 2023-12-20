d925587ba459dbde1f4ea3c88202afb537bae398
Soham Khedkar <134027325+soham-khedkar@users.noreply.github.com>
2023-12-20 13:04:31 +0000

    day 9

---
+++ 04_controlFlow/one.js
@@ -0,0 +1,44 @@
+//if 
+if(2 === '2'){
+ console.log("hi")
+}
+else {
+    //console.log("bye")
+}
+const score = 200
+if(score >100 ) {
+    let power = 'fly'
+    //console.log(`user power: ${power}`);
+
+}
+
+const balance = 1000
+//if(balance>500) console.log("test")
+//else console.log("hi");
+
+
+// if (balance < 500) {
+//     console.log("less than 500");
+// } else if (balance < 750) {
+//     console.log("less than 750");
+    
+// } else if (balance < 900) {
+//     console.log("less than 750");
+    
+// } else {
+//     console.log("less than 1200");
+
+// }
+
+const userLoggedIn = true
+const debitCard = true
+const loggedInFromGoogle = false
+const loggedInFromEmail = true
+
+if (userLoggedIn && debitCard && 2==3) {
+    console.log("Allow to buy course");
+}
+
+if (loggedInFromGoogle || loggedInFromEmail) {
+    console.log("User logged in");
+}
